fb6d7ad7e68fda6be62cdb3de35ce75c99cefa45
luisdbb <92033626+luisdbb@users.noreply.github.com>
2022-11-22 14:47:15 +0100

    Update MiClase.java

--- proyectoS/src/principal/MiClase.java
+++ proyectoS/src/principal/MiClase.java
@@ -3,7 +3,7 @@ package principal;
 public class MiClase {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from out of the world!");
+		System.out.println("Hello\n\tfrom\n\t\tout\n\t\t\tof\n\t\t\t\tthe\n\t\t\t\t\tworld!");
 
 	}
 
